bd993da084660743f7cdc197f526a79e04233506
JeffUyekawa <99925970+JeffUyekawa@users.noreply.github.com>
2023-06-25 12:40:09 -0700

    Add files via upload

--- Code/AddGCCandClean.py
+++ Code/AddGCCandClean.py
@@ -3,7 +3,7 @@
 import numpy as np
 import pandas as pd
 #Read in Site data csv
-df = pd.read_csv(r"C:\Users\jeffu\Documents\44Datasets.csv\44Datasets.csv", header = 0, low_memory=False)
+df = pd.read_csv(r"C:\Users\jeffu\Documents\44DatasetsLE.csv\44DatasetsLE.csv", header = 0, low_memory=False)
 uni = list(np.unique(df['Site']))
 #Read in GCC csv
 df2 = pd.read_csv(r"C:\Users\jeffu\OneDrive\Documents\43GCCData.csv", header =0, low_memory=False)
@@ -27,11 +27,11 @@ for site in uni:
 cleandflist = []
 for i,df in enumerate(gcclist):
     print(f'Attempting to clean DF{i+1}')
-    df['YDOY'] = df['year'].astype(str)+ df['doy'].astype(str)
+    df.loc[:,'YDOY'] = df.loc[:,'year'].astype(str)+ df.loc[:,'doy'].astype(str)
     print(f'DF{i+1} -- Complete')
     cleandflist.append(df.loc[:,['YDOY','midday_gcc','midday_rcc','gcc_50','gcc_75','gcc_90','rcc_50','rcc_75','rcc_90']])
 for df in dflist:
-    df['YDOY'] = df['YEAR'].astype(str)+ df['DOY'].astype(str)
+    df.loc[:,'YDOY'] = df.loc[:,'YEAR'].astype(str)+ df.loc[:,'DOY'].astype(str)
 
 
 #%%
@@ -58,16 +58,13 @@ final_df.drop(['index'], axis = 1, inplace = True)
 del merge_list, cleandflist, gcclist, dflist, df, df2
 #%%
 #Columns to keep
-keep_cols = ['TS_1_1_1',
+keep_cols = ['DOY', 'HOUR', 'MONTH','DAY', 'YEAR',
+'TS_1_1_1',
  'TS_1_2_1',
 'PPFD',
  'Tair',
  'VPD',
  'SWC_1_1_1',
- 'HourCos',
- 'HourSin',
- 'DOYCos',
- 'DOYSin',
  'FC50',
  'LE50',
  'Site',
@@ -86,18 +83,31 @@ keep_cols = ['TS_1_1_1',
  'gcc_90',
  'rcc_50',
  'rcc_75',
- 'rcc_90',
- 'V2'
+ 'rcc_90'
  ]
-#Create FC50 
-final_df = final_df.rename(columns={"V2": "LE"}) # LE gets renamed upon addition. 
+#Create FC50 and LE50
 data = final_df[final_df.Ustar >= final_df.U50]
+
 final_df["FC50"] = data["FC"]
-final_df["LE50"] = data["LE"]
+final_df["LE50"] = data["V2"]
 
 #Final df of kept columns
 final_df = final_df.loc[:,keep_cols]
-# %%
+#%%
+#Add in phenocam site metadata 
+new_cols = pd.read_csv(r"C:\Users\jeffu\OneDrive\Documents\Newcolumns.csv")
+new_cols.rename(columns={'flux_sitenames':'Site'},inplace=True)
+final_df = final_df.merge(new_cols, how='left', on='Site')
+
+#Subtract 1 from DOY on Leap Year
+final_df.loc[(final_df['YEAR']==2020) & (final_df['DOY']>=60),'DOY'] -=1
+
+#Scale DOY and HOUR
+final_df.loc[:,'DOY'] /= 365
+final_df.loc[:,'HOUR'] /= 24
+#Remove Month,day,year
+final_df.drop(['MONTH','DAY','YEAR'], axis = 1, inplace= True)
+
 #Remove FC50 outliers
 def outlier(data):
     data.dropna(inplace=True)
@@ -111,22 +121,41 @@ def outlier(data):
 
 
 x = outlier(final_df["FC50"])
-d = final_df[final_df["FC50"] <= x[1]]
+dFC = final_df[final_df["FC50"] <= x[1]]
 
-d = d[d["FC50"] >= x[0]]
+dFC = dFC[dFC["FC50"] >= x[0]]
 
-dpreds = d.drop(["FC50","LE50"], axis = 1)
-dresponse = d["FC50","LE50"]
+#Remove LE50 outliers separately 
+x = outlier(final_df["LE50"])
+dLE= final_df[final_df["LE50"] <= x[1]]
 
-
+dLE = dLE[dLE["LE50"] >= x[0]]
+#%%
+#Interpolate and create FC50 Dataset
+dpreds = dFC.drop(["FC50","LE50"], axis = 1)
+dresponse = dFC["FC50"]
 dpreds = dpreds.interpolate()
 
 dat = dpreds.join(dresponse)
+dat= dat.join(pd.get_dummies(dat.primary_veg_type, prefix='PVeg'))
+dat = dat.join(pd.get_dummies(dat.secondary_veg_type,prefix='SVeg'))
+dat.drop(['primary_veg_type','secondary_veg_type'],axis=1,inplace=True)
+dfFC50 = dat.dropna()
 
-#Drop NA values
-df2 = dat.dropna()
+
+#Interpolate and create LE50 Dataset
+dpreds = dLE.drop(["FC50","LE50"], axis = 1)
+dresponse = dLE["LE50"]
+dpreds = dpreds.interpolate()
+
+dat = dpreds.join(dresponse)
+dat= dat.join(pd.get_dummies(dat.primary_veg_type, prefix='PVeg'))
+dat = dat.join(pd.get_dummies(dat.secondary_veg_type,prefix='SVeg'))
+dat.drop(['primary_veg_type','secondary_veg_type'],axis=1,inplace=True)
+dfLE50 = dat.dropna()
+
 #%%
 #Save to csv. Change file path
-df2.to_csv(r"C:\Users\jeffu\Documents\Condensed43+GCC.csv")
-
+dfFC50.to_csv(r"C:\Users\jeffu\Documents\FC50Data.csv",index=False)
+dfLE50.to_csv(r"C:\Users\jeffu\Documents\LE50Data.csv",index=False)
 # %%
